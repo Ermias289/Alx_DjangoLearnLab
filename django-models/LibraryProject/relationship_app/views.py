652cfcfef19a0b04ece55e39ab786357fd713dfe
Jermiah Molla <ermiasmolla00@gmail.com>
2025-09-07 20:45:03 +0300

    dag

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Library
 from .models import Book
+from django.http import HttpResponse
+from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # create a plain text string with titles and authors
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
 
 class LibraryDetailView(DetailView):
     model = Library
